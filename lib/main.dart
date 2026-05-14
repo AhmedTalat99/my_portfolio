7394891464c69d7febe08cf3b8e099fe18811eb8
Ahmed Talat <121727649+AhmedTalat99@users.noreply.github.com>
2026-05-14 17:37:55 +0300

    Add global config and model files

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/views/main_dashboard.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final PageStorageBucket bucket = PageStorageBucket();
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PageStorage(
+        bucket: bucket,
+        child: const MainDashBoard(),
+      ),
+    );
+  }
+}
